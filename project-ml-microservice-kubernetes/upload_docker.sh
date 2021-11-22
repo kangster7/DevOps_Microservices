ed035135763a8ba56afe3089fc09b836ba4be0ce
EC2 Default User <ec2-user@ip-172-31-16-90.us-west-1.compute.internal>
2021-11-22 20:46:51 +0000

    adding docker image

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -7,9 +7,14 @@
 # Create dockerpath
 # dockerpath=<your docker ID/path>
 
+dockerpath=kangster7/project4
+
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u kangster7
 
 # Step 3:
 # Push image to a docker repository
+docker tag project4 $dockerpath:latest
+docker push $dockerpath:latest
